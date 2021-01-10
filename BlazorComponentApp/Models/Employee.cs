7f685272a4e98e7fee3c6489a8e1764c8315a221
chisoftltd <chisoftltd@gmail.com>
2021-01-10 20:29:21 +0100

    Avoiding Data Initialization Pitfall

---
+++ BlazorComponentApp/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorComponentApp.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Department { get; set; }
+
+        public void LoadRecords()
+        {
+        // Coming soon...
+        }   
+    }
+
+    
+}
